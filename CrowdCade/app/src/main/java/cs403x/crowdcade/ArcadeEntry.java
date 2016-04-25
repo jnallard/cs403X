baf9767df4fa764ea68a0e77fab249aa77a23b8a
jnallard <jnallard@wpi.edu>
2016-04-25 16:22:01 -0400

    Adds NetworkManager and ArcadeEntry

---
+++ CrowdCade/app/src/main/java/cs403x/crowdcade/ArcadeEntry.java
@@ -0,0 +1,56 @@
+package cs403x.crowdcade;
+
+import android.location.Location;
+
+/**
+ * Created by Joshua on 4/25/2016.
+ */
+public class ArcadeEntry {
+    String name;
+    String locationName;
+    String address;
+    Location location;
+    double rating;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getLocationName() {
+        return locationName;
+    }
+
+    public void setLocationName(String locationName) {
+        this.locationName = locationName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Location getLocation() {
+        return location;
+    }
+
+    public void setLocation(Location location) {
+        this.location = location;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
+
+}
---
+++ CrowdCade/app/src/main/java/cs403x/crowdcade/NetworkManager.java
@@ -0,0 +1,33 @@
+package cs403x.crowdcade;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Joshua on 4/25/2016.
+ */
+public class NetworkManager {
+
+    private NetworkManager instance = new NetworkManager();
+
+    private NetworkManager(){
+
+    }
+
+    public NetworkManager getInstance(){
+        return instance;
+    }
+
+    public List<ArcadeEntry> getArcadeEntries(){
+        return new ArrayList<>();
+    }
+
+    public void reportArcadeEntry(ArcadeEntry entry){
+
+    }
+
+    public void reportArcadeEntryVisited(ArcadeEntry entry){
+
+    }
+
+}
